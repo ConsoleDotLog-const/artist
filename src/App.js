c5d7a2ac4ae1b277767ec5ee3a374ada91b6750e
ConsoleDotLog-const <c-gusto@hotmail.com>
2020-11-24 09:23:52 +1300

    starting on heaer, materail UI added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import './App.css';
+import './App.scss';
+
+import Header from './components/header/Header'
+
+
 
 function App() {
   return (
     <div className="App">
-      hello
+      <div className="container">
+       <Header/>
+        <section className="features">
+          Features 
+
+        </section>
+
+        <div className="story__content">
+          Story
+        </div>
+
+        <div className="story__img">
+        Image
+        </div>
+
+        <div className="homes">
+
+        </div>
+
+      </div>
+      
     </div>
   );
 }
