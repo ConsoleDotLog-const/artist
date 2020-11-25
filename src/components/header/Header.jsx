aae1ca0ea10fd0757ef7b8fd577759fabd1154cc
ConsoleDotLog-const <c-gusto@hotmail.com>
2020-11-25 17:26:58 +1300

    nav bar and side bar created in header, material UI core/icons installed again due to bug

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,69 @@
 import React, { Component } from 'react';
 
+
+import AppBar from '@material-ui/core/AppBar'
+import ToolBar from '@material-ui/core/Toolbar'
+import MenuIcon from '@material-ui/icons/Menu'
+import IconButton from '@material-ui/core/Icon'
+
+import SideDrawer from './sideDrawer'
+
+
 class Header extends Component {
+
+    state={
+        drawerOpen: false
+    }
+
+    toggleDrawer = (value)=>{
+        this.setState({
+            drawerOpen: value
+        })
+    }
+
     render() {
         return (
             <div className="header">
-                
+                <AppBar
+                    position="fixed"
+                    style={{
+                        backgroundColor:"#2f2f2f",
+                        boxShadow:'none',
+                        padding: '10px 0px'
+                    }}
+                >
+                   <ToolBar>
+
+                       <div className="header__logo">
+                            <div className='heading-1'
+                                style={{
+                                    textTransform:"uppercase"
+                                }}
+                            >Youmii</div>
+                            <div className='heading-3'>Design Studios</div>
+                       </div>
+
+                       <IconButton
+                        aria-label="menu"
+                        color="inherit"
+                        onClick={()=> this.toggleDrawer(true) }
+                        display="block"
+                        
+                       >
+                           <MenuIcon/>
+                       </IconButton>
+
+                       <SideDrawer
+                        open={this.state.drawerOpen}
+                        onClose={(value)=> this.toggleDrawer(value)}
+                       >
+
+
+                       </SideDrawer>
+                     
+                   </ToolBar>
+                   
+                </AppBar>
             </div>
         );
     }
---
+++ src/components/header/sideDrawer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import Drawer from '@material-ui/core/Drawer'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+
+const SideDrawer = (props) => {
+    return (
+        <Drawer
+            anchor="right"
+            open={props.open}
+            onClose={()=> props.onClose(false) }
+        >
+            <List component="nav">
+                <ListItem button onClick={()=>console.log("Heading")} className="heading-3">
+                   Heading
+                </ListItem>
+                <ListItem button onClick={()=>console.log("Featured")} className="heading-3">
+                    Featured
+                </ListItem>
+                <ListItem button onClick={()=>console.log("Highlights")} className="heading-3">
+                    Highlights
+                </ListItem>
+                <ListItem button onClick={()=>console.log("Pricing")} className="heading-3">
+                    Pricing
+                </ListItem>
+                <ListItem button onClick={()=>console.log("Location")} className="heading-3">
+                    Location
+                </ListItem>
+
+            </List>
+        </Drawer>
+    );
+};
+
+export default SideDrawer;
